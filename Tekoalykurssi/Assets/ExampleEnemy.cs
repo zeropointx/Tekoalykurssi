1dfa0ed1a09d0111212bde3f3f50fe0414a897e0
zeropointx <tti13smiikas@kamk.fi>
2015-03-30 18:52:53 +0300

    Added example enemy and first person character

---
+++ Tekoalykurssi/Assets/ExampleEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleEnemy : MonoBehaviour {
+    Transform player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Vector3.Distance(transform.position,player.position) > 2.0f)
+    {
+        this.transform.position += (player.position - transform.position).normalized * Time.deltaTime;
+    }
+	}
+}
