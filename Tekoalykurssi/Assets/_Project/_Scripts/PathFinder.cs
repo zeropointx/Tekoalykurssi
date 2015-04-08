0ad3271880f52429f805401951a5b4c624ef4f03
havytomn <tti13smiikas@kamk.fi>
2015-04-08 15:49:42 +0300

    Added pathfinderscript, moved scripts to script folder and pappa...

---
+++ Tekoalykurssi/Assets/_Project/_Scripts/PathFinder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class PathFinder : MonoBehaviour {
+    List<Vector3> waypoints = new List<Vector3>();
+	// Use this for initialization
+	void Start () {
+	    for(int i = 0; i <  GameObject.Find("PathMap").transform.childCount; i++)
+        {
+            waypoints.Add(GameObject.Find("PathMap").transform.GetChild(i).position);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
